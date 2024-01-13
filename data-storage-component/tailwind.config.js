6ecd6af9f6bc00aa94a7de8963c09667ad24b502
Terry Wambui <shikuwambui@gmail.com>
2024-01-13 17:30:29 +0300

    added custom styles

--- data-storage-component/tailwind.config.js
+++ data-storage-component/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         "grayishBlue": "hsl(229, 7%, 55%)",
         "darkBlue": "hsl(228, 56%, 26%)",
         "veryDarkBlue": "hsl(229, 57%, 11%)",
-        "primaryGradient": "hsl(6, 100%, 80%) to hsl(335, 100%, 65%)",
+        "primaryGradient": " from hsl(6, 100%, 80%) to hsl(335, 100%, 65%)",
       },
       fontFamily: {
         body: ["Raleway, sans-serif"],
@@ -19,6 +19,12 @@ module.exports = {
       backgroundImage: {
         "desktop": "url(./public/images/bg-desktop.png)",
         "mobile": "url(./public/images/bg-mobile.png)"
+      },
+      borderRadius: {
+        'large' : '130px',
+      },
+      spacing: {
+        '600px': '600px',
       }
     },
   },
